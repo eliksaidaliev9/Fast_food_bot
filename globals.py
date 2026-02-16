5d3f8bb9bedc6ac3686fe9c8513ba762a74ba49c
Elyor <elik.saidaliev9@gmail.com>
2026-02-16 18:07:09 +0500

    Add complete Telegram food ordering bot with registration, cart logic, and admin notifications

---
+++ globals.py
@@ -0,0 +1,144 @@
+WELCOME_TEXT = "Xush kelibsiz! / Добро пожаловать!/ Welcome!"
+CHOOSE_LANG = "Tilni tanlang! / Выберите язык!/ Choose a language!"
+BTN_LANG_UZ = "Uzbek tili"
+BTN_LANG_RU = "Русский язык"
+BTN_LANG_EN = "English language"
+STATES = {
+    "reg": 1,
+    "menu": 2,
+    "settings": 3
+}
+LANGUAGE_CODE = {
+    1: "uz",
+    2: "ru",
+    3: "en"
+}
+TEXT_LANG_WARNING = "Iltimos tillardan birini tanlang!\nПожалуйста, выберите один из языков!\nPlease choose one of the languages!"
+TEXT_ENTER_FIRST_NAME = {
+    1: "Iltimos ismingizni kiriting!",
+    2: "Пожалуйста, введите ваше имя!",
+    3: "Please enter your name!"
+}
+TEXT_ENTER_LAST_NAME = {
+    1: "Iltimos familiyangizni kiriting!",
+    2: "Пожалуйста, введите свою фамилию!",
+    3: "Please enter your last name!"
+}
+BTN_SEND_CONTACT = {
+    1: "Yuborish",
+    2: "Отправить",
+    3: "Send"
+}
+TEXT_ENTER_CONTACT = {
+    1: "Iltimos telefon raqamingizni yuboring yoki <<Yuborish>> tugmasini bosing!",
+    2: "Отправьте свой номер телефона или нажмите кнопку <<Отправить>>!",
+    3: "Send your phone number or click the <<Send>> button!"
+}
+TEXT_MAIN_MENU = {
+    1: "🏠 Asosiy menyu",
+    2: "🏠 Главное меню",
+    3: "🏠 Main Menu"
+}
+BTN_ORDER = {
+    1: "🛒 Buyurtma qilish",
+    2: "🛒 Заказать",
+    3: "🛒 To order"
+}
+TEXT_ORDER = {
+    1: "Kategoriylardan birini tanlang!",
+    2: "Выбери одну из категорий!",
+    3: "Choose one of the categories!"
+}
+BTN_MY_ORDERS = {
+    1: "🛍 Buyurtmalarim",
+    2: "🛍 Мои заказы",
+    3: "🛍 My orders"
+}
+BTN_ABOUT_US = {
+    1: "👨‍👩‍👦 Biz haqimizda",
+    2: "👨‍👩‍👦 O нас",
+    3: "👨‍👩‍👦 About us"
+}
+BTN_KORZINKA = {
+    1: "🛍 Xarid qilish",
+    2: "🛍 Покупка товаров",
+    3: "🛍 Purchase of goods"
+}
+BTN_COMMENTS = {
+    1: "✍️ Fikr bildirish",
+    2: "✍️ комментарий",
+    3: "✍️ comment"
+}
+BTN_SETTINGS = {
+    1: "⚙️ Sozlamalar",
+    2: "⚙️ Настройки",
+    3: "⚙️ Settings"
+}
+
+########## lesson-3 ###########
+TEXT_PRODUCT_PRICE = {
+    1: "Narx:",
+    2: "Цена:",
+    3: "Price"
+}
+TEXT_PRODUCT_DESC = {
+    1: "Tarkibi:",
+    2: "Состав:",
+    3: "Composition"
+}
+###########################
+
+
+# lesson-4 ##############
+
+AT_KORZINKA = {
+    1: "Savatchada",
+    2: "В корзине",
+    3: "In the shopping cart"
+}
+
+ALL = {
+    1: "Jami",
+    2: "Все",
+    3: "All"
+}
+
+ZAKAZ = {
+    1: "Buyurtma",
+    2: "Заказ",
+    3: "Order"
+}
+
+SUM = {
+    1: "so'm",
+    2: "сум",
+    3: "sum"
+}
+
+ABOUT_COMPANY = {
+    1: "Biz <b>B-8.0 backend dasturchilar guruhi</b>miz! "
+         "\n\nBirgalikda biznesingizni rivojlantiramiz va uni "
+         "zamonaviy uslubda ommaga taqdim qilamiz!\n\n"
+         "<b>BIZ SIZ HAQINGIZDA QAYG'URAMIZ!!!</b>",
+    2: "Мы <b>B-8.0 backend dasturchilar guruhi</b>!"
+       "\n\nМы будем развивать ваш бизнес вместе"
+       "и представляем публике в современном стиле!\n\n"
+       "<b>МЫ ЗАБОТИМСЯ О ВАС !!!</b>",
+    3: "We are <b>B-8.0 backend dasturchilar guruhi</b>!"
+       "\n\nWe will develop your business together"
+       "and present it to the public in a modern style!\n\n"
+       "<b>WE TAKE CARE OF YOU !!!</b>"
+}
+
+NO_ZAKAZ = {
+    1: "Siz hali birorta buyurtma bermadingiz!",
+    2: "Вы еще не разместили ни одного заказа!",
+    3: "You haven't placed any orders yet!"
+}
+
+SEND_LOCATION = {
+    1: "📍 Lokatsiya jo'nating",
+    2: "📍 Отправить местоположение",
+    3: "📍 Send Location"
+}
+#########################33
